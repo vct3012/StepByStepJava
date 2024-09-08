054134f29441e45b09880d7ef6a2b9de0ba579e6
vongchutan3012 <vct3012.git@gmail.com>
2024-09-08 21:22:04 +0700

    add JAVA04_Variables.java

---
+++ src/TITV/JAVA04_Variables.java
@@ -0,0 +1,9 @@
+package TITV;
+
+public class JAVA04_Variables {
+    public static void main (String[] args) {
+        String thongBao = "Hello";
+        String hoVaTen;
+        hoVaTen = "Vong Chu Tan";
+    }
+}
