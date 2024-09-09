0ce70b1c2f5a153c4b4983af5b3a6e521a4d8539
vongchutan3012 <vct3012.git@gmail.com>
2024-09-09 22:36:44 +0700

    Sử dụng biểu thức điều kiện "?"

--- src/TITV/JAVA05_Types.java
+++ src/TITV/JAVA05_Types.java
@@ -1,30 +1,18 @@
 package TITV;
 
-import java.lang.reflect.Type;
-
 public class JAVA05_Types {
     public static void main(String[] args) {
-//        final byte tan = 1;
-//        System.out.println(tan);
-
         String tenHocSinh = "Vong Chu Tan";
         int namSinh = 2003;
         double kinhTe = 222.350;
         boolean conSong = true;
 
-
-        if (conSong == true) {
-            System.out.println("Con song");
-        }
-
         char Id = 'M';
-        if (Id == 'M') {
-            System.out.println("Nam");
-        }
 
+        // In thông tin của học sinh
         System.out.println("ten hoc sinh " + tenHocSinh
-        + " nam sinh " + namSinh + " Con song hay khong? " + conSong + " char" + Id );
-
-
+                + " nam sinh " + namSinh
+                + " Con song hay khong? " + (conSong ? "Con song" : "Khong con song")
+                + " Gioi tinh: " + (Id == 'M' ? "Nam" : "Nu"));
     }
 }
