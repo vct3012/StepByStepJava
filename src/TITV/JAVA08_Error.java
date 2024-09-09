a7617186f3ec72e1af01b960dbfab4476e8d7d96
vongchutan3012 <vct3012.git@gmail.com>
2024-09-09 23:02:55 +0700

    Test error

--- src/TITV/JAVA08_Error.java
+++ src/TITV/JAVA08_Error.java
@@ -4,7 +4,7 @@ public class JAVA08_Error {
     public static void main(String[] args) {
         System.out.println("Hello World");
         final int x= 10;
-        x = 9;
+        //x = 9;
         //home/tan/Desktop/Coding/StepByStepJava/src/TITV/JAVA08_Error.java:7:9
         //java: cannot assign a value to final variable x
 
