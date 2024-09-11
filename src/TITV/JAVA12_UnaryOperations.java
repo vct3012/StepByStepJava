ac33e5f4b628c471cec5077043762c633f450bd5
vongchutan3012 <vct3012.git@gmail.com>
2024-09-11 23:08:03 +0700

    Đã hiểu, bước từng bước

---
+++ src/TITV/JAVA12_UnaryOperations.java
@@ -0,0 +1,17 @@
+package TITV;
+
+public class JAVA12_UnaryOperations {
+    public static void main(String[] args) {
+        int a= 5;
+        boolean b= false;
+        System.out.println("test: " + a++ ); // In ra a roi cong / =6
+        System.out.println("test: " + a-- ); // In ra a roi tru / =5
+
+        System.out.println("---------------------");
+        System.out.println("Gia tri phep cong: " + (++a)); //cong roi in ra / =6
+        System.out.println("Gia tri phep tru: " + (--a)); // =5
+        System.out.println("Gia tri cua bien b la: " + (!b));
+
+
+    }
+}
