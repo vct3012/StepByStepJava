7ab65e8be6fbcd822f3c6420be92e6be9eb81088
vongchutan3012 <vct3012.git@gmail.com>
2024-09-11 23:55:18 +0700

    Added JAVA13_Assignment.java

---
+++ src/TITV/JAVA13_Assignment.java
@@ -0,0 +1,16 @@
+package TITV;
+
+import java.util.Scanner;
+
+public class JAVA13_Assignment {
+    public static void main(String[] args) {
+        Scanner NTBP = new Scanner(System.in);
+        float a = 5;
+        System.out.println("Enter the value of a: ");
+        a= NTBP.nextFloat();
+        System.out.println("a" + a );
+        a+=3;
+        System.out.println("a"+a);
+
+    }
+}
