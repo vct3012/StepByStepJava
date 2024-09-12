bcabedc4e5d3b4c04a0b354d0ded2684062c7e9d
vongchutan3012 <vct3012.git@gmail.com>
2024-09-13 00:02:10 +0700

    first coparison

--- src/TITV/JAVA13_Comparison.java
+++ src/TITV/JAVA13_Comparison.java
@@ -1,4 +1,17 @@
 package TITV;
 
+import java.util.Scanner;
+
 public class JAVA13_Comparison {
+    public static void main(String[] args) {
+        Scanner NTPB = new Scanner(System.in);
+        int a,b;
+        System.out.println("Nhap a = ");
+        a = NTPB.nextInt();
+        System.out.println("Nhap b = ");
+        b = NTPB.nextInt();
+
+        //Xuat ra kqua so sanh
+        System.out.println(a + " = " +b + (a==b) );
+    }
 }
