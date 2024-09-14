ed12414fe1bbe3b15bbb25883ec9526528bf5377
vongchutan3012 <vct3012.git@gmail.com>
2024-09-14 22:23:51 +0700

    Change the name of Java14

---
+++ src/TITV/JAVA14_Comparison.java
@@ -0,0 +1,24 @@
+package TITV;
+
+import java.util.Scanner;
+
+public class JAVA14_Comparison {
+    public static void main(String[] args) {
+        Scanner NTPB = new Scanner(System.in);
+        float c;
+        System.out.println("Nhap c = ");
+        c = NTPB.nextFloat();
+        System.out.println("c = " + c);
+
+        c += 3;
+        System.out.println("c2 = " + c);
+        c -= 2;
+        System.out.println("c3 = " + c);
+        c /= 3;
+        System.out.println("c4 = " + c);
+        c %= 3;
+        System.out.println("c5 = " + c);
+
+
+    }
+}
