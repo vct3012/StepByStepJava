8a9fc94e447cd1ed8e672817bcf435417ffd3ffd
vongchutan3012 <vct3012.git@gmail.com>
2024-09-14 23:40:14 +0700

    Boolean Expression

--- src/TITV/JAVA15_BooleanExpression.java
+++ src/TITV/JAVA15_BooleanExpression.java
@@ -1,4 +1,14 @@
 package TITV;
 
+import java.util.Scanner;
+
 public class JAVA15_BooleanExpression {
+    public static void main(String[] args) {
+        Scanner NTBP = new Scanner(System.in);
+        System.out.println("Nhap a = ");
+        int a = NTBP.nextInt();
+
+        String ketQua = (a%2==0)?"So chan":"so le";
+        System.out.println(a + " la " +  ketQua);
+    }
 }
