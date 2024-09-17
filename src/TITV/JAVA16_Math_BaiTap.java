cc428836cf58cd086c3cfb880a10be98ac6dcc14
vongchutan3012 <vct3012.git@gmail.com>
2024-09-17 21:29:40 +0700

    Done practice

--- src/TITV/JAVA16_Math_BaiTap.java
+++ src/TITV/JAVA16_Math_BaiTap.java
@@ -1,4 +1,19 @@
 package TITV;
 
+import java.util.Scanner;
+
 public class JAVA16_Math_BaiTap {
+    public static void main(String[] args) {
+        double r, dienTich, chuVi;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao ban kinh hinh tron: ");
+        r = sc.nextDouble();
+
+        chuVi = r * 2 * Math.PI;
+        System.out.println("chu vi hinh tron la " + Math.round(chuVi));
+
+        dienTich = Math.PI * Math.pow(r, 2);
+        System.out.println("dien tich hinh tron la " + Math.round(dienTich));
+
+    }
 }
