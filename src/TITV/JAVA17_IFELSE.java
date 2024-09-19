376eb07cefa172f22b75ebf7b19b160564b39601
vongchutan3012 <vct3012.git@gmail.com>
2024-09-19 23:18:43 +0700

    Added JAVA17_IFELSE

---
+++ src/TITV/JAVA17_IFELSE.java
@@ -0,0 +1,19 @@
+package TITV;
+
+import java.util.Scanner;
+
+public class JAVA17_IFELSE {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap so n vao: ");
+        n = scanner.nextInt();
+
+        //If kieu tra chan le
+        if(n%2 == 0){
+            System.out.println(n + " n la so chan");
+        }else {
+            System.out.println(n + " n la so le");
+        }
+    }
+}
