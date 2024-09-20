a154c6dbac1ab11f769b36cb642195ae87dd2f22
vongchutan3012 <vct3012.git@gmail.com>
2024-09-20 23:30:06 +0700

    Done JAVA20_SwitchCase_Practice.java

---
+++ src/TITV/JAVA20_SwitchCase_Practice.java
@@ -0,0 +1,44 @@
+package TITV;
+
+import java.util.Scanner;
+
+public class JAVA20_SwitchCase_Practice {
+    public static void main(String[] args) {
+
+            int thang, nam;
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Nhap vao thang: ");
+            thang = sc.nextInt();
+            System.out.println("Nhap vao nam: ");
+            nam = sc.nextInt();
+
+            switch (thang) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    System.out.println("Có 31 ngày");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println("Có 30 ngày");
+                    break;
+                case 2:
+                    if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0)) {
+                        System.out.println("Có 29 ngày");
+                    } else {
+                        System.out.println("Có 28 ngày");
+                    }
+                    break;
+                default:
+                    System.out.println("Nhập dữ liệu sai!");
+                    break;
+            }
+        }
+    }
