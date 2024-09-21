e59c054da224f131b6cf28384eb4a719775332c8
vongchutan3012 <vct3012.git@gmail.com>
2024-09-21 14:23:58 +0700

    Rename Java22 to Java23

--- src/TITV/JAVA22_For_Practice.java
+++ src/TITV/JAVA22_For_Practice.java
@@ -1,6 +1,6 @@
 package TITV;
 
-public class JAVA21_For_Practice {
+public class JAVA22_For_Practice {
     public static void main(String[] args) {
         for (int i = 2; i <= 11 ; i++) {
             for (int j = 1; j <=100  ; j++) {
