31aed09617295c32b898b76d0f1298d4f5cd3621
vongchutan3012 <vct3012.git@gmail.com>
2024-09-21 14:08:51 +0700

    Done JAVA21_For.java

--- src/TITV/JAVA21_For.java
+++ src/TITV/JAVA21_For.java
@@ -3,5 +3,9 @@ package TITV;
 public class JAVA21_For {
     public static void main(String[] args) {
 
+        for (int i = 200; i > 20; i--) {
+            System.out.println(i + " In vong lap for");
+
+        }
     }
 }
