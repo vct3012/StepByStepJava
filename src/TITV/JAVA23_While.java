6bad6a2023b281439c1d9c48ea93385f12615dc8
vongchutan3012 <vct3012.git@gmail.com>
2024-09-21 14:54:02 +0700

    Done JAVA23_While.java

---
+++ src/TITV/JAVA23_While.java
@@ -0,0 +1,25 @@
+package TITV;
+
+import java.util.Scanner;
+
+public class JAVA23_While {
+    public static void main(String[] args) {
+//        int i = 1;
+//        Scanner sc = new Scanner(System.in);
+//        while( i!=0 ){
+//            System.out.println(" Nhap ki tu i vao, ki tu =0 thi se thoat");
+//            i = sc.nextInt();
+//        }
+
+        int x = 0;
+        while(true){
+            System.out.println(x);
+            x++;
+//            if (x==10){
+//                break;
+            }
+        }
+
+
+    }
+
