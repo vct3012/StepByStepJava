b14990d64b1df41ba47df21d6c7f8d97fe520af5
vongchutan3012 <vct3012.git@gmail.com>
2024-10-02 13:34:59 +0700

    Added JAVA26_DoWhile.java

--- src/TITV/JAVA26_DoWhile.java
+++ src/TITV/JAVA26_DoWhile.java
@@ -1,4 +1,27 @@
 package TITV;
 
 public class JAVA26_DoWhile {
+//    public static void main(String[] args) {
+//        int out, in = 0;
+//        for (out = 0; out < 10; out++) {
+//            for (in = 0; in < 20; in++) {
+//                if (in > 10)
+//                    break;
+//            }
+//            System.out.println("ben trong vong lap out: out = " + out + ", in = " + in);
+//        }
+//        System.out.println("ben ngoai vong lap out: out = " + out + ", in = " + in);
+//    }
+
+    public static void main(String[] args) {
+        int out, in = 0;
+        outer: for (out = 0; out < 10; out++) {
+            for (in = 0; in < 20; in++) {
+                if (in > 10)
+                    break outer;
+            }
+            System.out.println("ben trong vong lap out: out = " + out + ", in = " + in);
+        }
+        System.out.println("ben ngoai vong lap out: out = " + out + ", in = " + in);
+    }
 }
