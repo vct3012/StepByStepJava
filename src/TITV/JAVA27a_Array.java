eba2d43da07a2e476ea03b9d1f3b4a15a5dc6f49
chutan <vct3012.git@gmail.com>
2024-10-13 12:06:22 -0400

    From fedora

--- src/TITV/JAVA27a_Array.java
+++ src/TITV/JAVA27a_Array.java
@@ -1,4 +1,24 @@
 package TITV;
 
+import java.util.Scanner;
+
 public class JAVA27a_Array {
+    public static void main(String[] args) {
+        double[] mang1;
+        double[] mang2;
+
+        mang1 = new double[10];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < mang1.length; i++) {
+            System.out.println("nhap phan tu thu"+i+":");
+            mang1[i] = sc.nextDouble();
+
+        }
+        double tong = 0;
+        for (int i = 0; i < mang1.length; i++) {
+            tong += mang1[i];
+
+        }
+        System.out.println("Tong = " + tong);
+    }
 }
